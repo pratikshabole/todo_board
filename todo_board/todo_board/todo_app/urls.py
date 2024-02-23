c9e233f6f2334d1562e6369b0f74dfe76d814a85
pratikshabole <120154388+pratikshabole@users.noreply.github.com>
2024-02-23 12:47:32 +0530

    Update urls.py

--- todo_board/todo_board/todo_app/urls.py
+++ todo_board/todo_board/todo_app/urls.py
@@ -2,17 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from .views import TaskListView, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, ListCreate
+# , completedtasks
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.loginpage, name='login'),
-    path('', TaskListView.as_view(), name='tasklistview'),
+    path('', views.loginpage, name='login'),
+    path('task-view', TaskListView.as_view(), name='tasklistview'),
     path('logout/', views.logoutpage, name='logout'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('list-create/', ListCreate.as_view(), name='list-create'),
     path('create/', TaskCreate.as_view(), name='create'),
     path('update/<int:pk>/', TaskUpdate.as_view(), name='update'),
     path('delete/<int:pk>/', TaskDelete.as_view(), name='delete'),
+    # path('completed-tasks/', views.completedtasks, name='completed-task')
 
 
 ]
